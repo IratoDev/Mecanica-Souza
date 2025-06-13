77877f37cb1779cce171607b40747f3ef64298e7
IratoDev <iratodev.frontend@gmail.com>
2025-06-13 18:01:39 -0300

    correção do swiper tipagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import AppRouter from "./router"
 import "./GloablStyle.css"
-
+import "swiper/css";
+import "swiper/css/effect-fade";
+ import "swiper/css/navigation"; 
+import "swiper/css/pagination";
 
 function App() {
 
